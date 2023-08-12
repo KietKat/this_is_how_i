1980d21d6de1da34dd3a28d372cc2f931dceba5a
Kyle Nguyen <kiet54dd@gmail.com>
2023-08-12 02:10:03 -0700

    test

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,35 +1,17 @@
 import bg from '../assets/bg.png';
-import chef from '../assets/chef.png';
 import dscat from '../assets/dscat.jpg';
 import mathcat from '../assets/mathcat.jpg';
-import gallery03 from '../assets/gallery03.png';
-import gallery04 from '../assets/gallery04.png';
 import trident from '../assets/trident.jpg';
-import logo from '../assets/logo.png';
-import menu from '../assets/menu.png';
-import overlaybg from '../assets/overlaybg.png';
 import babem from '../assets/ba bem.jpg';
-import findus from '../assets/findus.png';
 import cscat from '../assets/cscat.jpg';
-import sign from '../assets/sign.png';
-import quote from '../assets/quote.png';
 import tihiu from '../assets/TIHIU.jpg';
 
 export default {
   bg,
-  chef,
   dscat,
   mathcat,
-  gallery03,
-  gallery04,
   trident,
-  logo,
-  menu,
-  overlaybg,
   babem,
-  findus,
   cscat,
-  sign,
-  quote,
   tihiu,
 };
